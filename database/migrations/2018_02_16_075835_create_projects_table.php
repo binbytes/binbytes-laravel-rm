abc50d76d525d7d12413aafb6fe19f9dd7827e41
Kanetiya Nikunj <kanetiya@gmail.com>
2018-02-16 13:33:12 +0530

    Create migration to create projects table with basic fields

---
+++ database/migrations/2018_02_16_075835_create_projects_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('description', 3000);
+            $table->unsignedInteger('client_id');
+            $table->boolean('is_running');
+            //
+            $table->text('remarks')->nullable();
+            $table->timestamps();
+
+            $table->foreign('client_id')
+                ->references('id')->on('clients')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
