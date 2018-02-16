1f532da43298d14024e6be38f69b8b10e1bea66d
Kanetiya Nikunj <kanetiya@gmail.com>
2018-02-16 13:27:50 +0530

    Create migration to create clients table with basic fields

---
+++ database/migrations/2018_02_16_075514_create_clients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('image')->nullable();
+            //
+            $table->string('skype')->nullable();
+            $table->string('email')->nullable();
+            $table->string('linkedin')->nullable();
+            $table->string('twitter')->nullable();
+            //
+            $table->string('remarks', 3000)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
