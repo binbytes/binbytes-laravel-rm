c17e0a157d27f58cf9a95b96b938a3d77725836c
Kanetiya Nikunj <kanetiya@gmail.com>
2018-02-16 14:51:30 +0530

    Create model & factory for client & project

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\Project::class, function (Faker $faker) {
+    return [
+        'title' => $faker->name,
+        'description' => $faker->sentence,
+        'client_id' => function () {
+            return factory(App\Client::class)->create()->id;
+        },
+        'is_running' => true,
+        'remarks' => $faker->paragraph
+    ];
+});
