a6e2e23e6cf619a8b7e77d8b227d6e3dcca359b6
Kanetiya Nikunj <kanetiya@gmail.com>
2018-02-22 20:24:01 +0530

    Fix typo

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card card-default">
-                    <div class="card-header">Login</div>
+                    <div class="card-header">User</div>
 
                     <div class="card-body">
                         <form method="POST" enctype="multipart/form-data" action="{{ route('users.store') }}">
