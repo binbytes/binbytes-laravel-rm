3915bdbe6cb9abb377f666624a357a9e5093d954
Kanetiya Nikunj <kanetiya@gmail.com>
2018-02-22 22:59:58 +0530

    Update naming

--- tests/Feature/ClientTest.php
+++ tests/Feature/ClientTest.php
@@ -7,7 +7,7 @@ use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
-class ClientsTest extends TestCase
+class ClientTest extends TestCase
 {
     use RefreshDatabase;
 
