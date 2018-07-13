4d28b7ce7feb3403bba661bd456c1b8d90a3eae7
Nikunj Kanetiya <kanetiya@gmail.com>
2018-07-13 17:36:19 +0530

    Add slug for project

--- app/Project.php
+++ app/Project.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Project extends Model
 {
+    use HasSlug;
+
     protected $fillable = [
         'title', 'description', 'client_id', 'started_at', 'is_completed', 'slug', 'remarks'
     ];
@@ -13,6 +17,16 @@ class Project extends Model
     public function path()
     {
         return '/projects/' . $this->id;
+    }
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions()
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
     }
 
     public function client()
