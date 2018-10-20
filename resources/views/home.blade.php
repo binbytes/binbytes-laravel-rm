620f56805c529f7244ab549dbbe1c6dcb05a1798
Nikunj Kanetiya <kanetiya@gmail.com>
2018-10-21 00:20:36 +0530

    Update dashboard design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,17 @@
-@extends('layouts.app')
+@extends('layouts.app', [
+    'pageTitle' => 'Dashboard'
+])
 
 @section('content')
-<div class="container">
+<div class="container pg-dashboard">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card card-default">
-                <div class="card-header border-bottom">
-                    Dashboard
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header border-bottom pb-0">
+                    <h6 class="font-weight-bold">Today</h6>
                 </div>
-
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Welcome...
+                    <timer :initial-time="{{ auth()->user()->today_attendance->totaltime }}"></timer>
                 </div>
             </div>
         </div>
