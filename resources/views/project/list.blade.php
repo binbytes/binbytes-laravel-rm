c618b8732140347d42470b2b4453bd0dc5e5972c
Nikunj Kanetiya <kanetiya@gmail.com>
2018-10-21 00:01:19 +0530

    update project list page

--- resources/views/project/list.blade.php
+++ resources/views/project/list.blade.php
@@ -5,40 +5,46 @@
 @section('content')
     <div class="row">
         <div class="col">
-            <div class="card card-small mb-4">
-                <div class="card-header border-bottom">
-                    <a href="/projects/create" class="btn btn-primary pull-right">
-                        <i class="fa fa-plus mr-2"></i>
-                        Add Project
-                    </a>
-                </div>
+            <div class="mb-5">
+                <a href="/projects/create" class="btn btn-primary pull-right">
+                    <i class="fa fa-plus mr-2"></i>
+                    Add Project
+                </a>
+            </div>
 
-                <div class="card-body">
-                    @foreach($projects->chunk(3) as $chunkProjects)
-                        <div class="card-deck">
-                            @foreach($chunkProjects as $project)
-                                <div class="col-sm-4">
-                                    <div class="card mb-3">
-                                        <div class="card-body">
-                                            <a href="{{ $project->path() }}">
-                                                <h4 class="card-title">{{ $project->title }}</h4>
-                                            </a>
-                                            <p class="card-text">{{ $project->description }}</p>
-                                            @if($project->users)
-                                                <p class="card-text">{!! $project->users->pluck('name')->implode('<br>') !!}</p>
-                                            @endif
-                                            <p class="card-text"><small class="text-muted">Created at {{ $project->created_at->diffForHumans() }}</small></p>
+            @foreach($projects->chunk(3) as $chunkProjects)
+                <div class="row">
+                    @foreach($chunkProjects as $project)
+                        <div class="col-sm-4">
+                            <div class="card mb-3">
+                                <div class="card-header pb-0 border-bottom">
+                                    <a href="{{ $project->path() }}">
+                                        <h4 class="card-title">{{ $project->title }}</h4>
+                                    </a>
+                                </div>
+                                <div class="card-body py-4 border-bottom">
+                                    <p class="card-text">{{ $project->description }}</p>
+                                </div>
+                                @if($project->users->count())
+                                    <div class="card-footer">
+                                        <div class="d-flex">
+                                            @foreach($project->users as $user)
+                                                @if($user->avatar)
+                                                    <img src="{{ $user->avatar_url }}" class="avatar mr-1">
+                                                @else
+                                                    <span class="user-placeholder mr-1">{{ substr($user->name, 0, 2) }}</span>
+                                                @endif
+                                            @endforeach
                                         </div>
                                     </div>
-                                </div>
-                            @endforeach
+                                @endif
+                            </div>
                         </div>
                     @endforeach
                 </div>
-                <div class="card-footer">
-                    {{ $projects->links() }}
-                </div>
-            </div>
+            @endforeach
+
+            {{ $projects->links() }}
         </div>
     </div>
 @endsection
