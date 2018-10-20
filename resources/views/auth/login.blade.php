15bb7e0aaf6b796ccf88adb04175035e817607bf
Nikunj Kanetiya <kanetiya@gmail.com>
2018-10-21 00:14:28 +0530

    Update login page design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
                     <div class="form-group row">
                         <label for="email" class="col-sm-4 col-form-label text-md-right">Email</label>
 
-                        <div class="col-md-6">
+                        <div class="col-md-8">
                             <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
                             @if ($errors->has('email'))
@@ -28,7 +28,7 @@
                     <div class="form-group row">
                         <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
 
-                        <div class="col-md-6">
+                        <div class="col-md-8">
                             <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                             @if ($errors->has('password'))
@@ -40,22 +40,21 @@
                     </div>
 
                     <div class="form-group row">
-                        <div class="col-md-6 offset-md-4">
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                </label>
+                        <div class="col-md-8 offset-md-4">
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" id="remember" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <label class="custom-control-label" for="remember">Remember Me</label>
                             </div>
                         </div>
                     </div>
 
                     <div class="form-group row mb-0">
                         <div class="col-md-8 offset-md-4">
-                            <button type="submit" class="btn btn-primary">
+                            <button type="submit" class="btn btn-primary mb-2">
                                 Login
                             </button>
 
-                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                            <a class="btn pl-0 btn-link" href="{{ route('password.request') }}">
                                 Forgot Your Password?
                             </a>
                         </div>
