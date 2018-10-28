08e4e6a799bb6ebe2aa6f2ac68d52a1e1dd466ac
Nikunj Kanetiya <kanetiya@gmail.com>
2018-10-28 12:24:28 +0530

    Update projects list view to cards grid

--- resources/views/project/list.blade.php
+++ resources/views/project/list.blade.php
@@ -16,53 +16,49 @@
                 </div>
             @endcan
 
-            @foreach($projects->chunk(3) as $chunkProjects)
-                <div class="row">
-                    @foreach($chunkProjects as $project)
-                        <div class="col-sm-4">
-                            <div class="card mb-3">
-                                <div class="card-header pb-0 border-bottom d-flex justify-content-between">
-                                    <a href="{{ $project->path() }}">
-                                        <h4 class="card-title">{{ $project->title }}</h4>
+            <div class="card-columns">
+                @forelse($projects as $project)
+                    <div class="card mb-3">
+                        <div class="card-header pb-0 border-bottom d-flex justify-content-between">
+                            <a href="{{ $project->path() }}">
+                                <h4 class="card-title">{{ $project->title }}</h4>
+                            </a>
+                            <div class="d-flex">
+                                @can('update', $project)
+                                    <a class="btn btn-white" href="/projects/{{ $project->id }}/edit">
+                                        <i class="fas fa-edit"></i>
                                     </a>
-                                    <div class="d-flex">
-                                        @can('update', $project)
-                                            <a class="btn btn-white" href="/projects/{{ $project->id }}/edit">
-                                                <i class="fas fa-edit"></i>
-                                            </a>
-                                        @endcan
-                                        @can('delete', $project)
-                                            {{ html()->form('DELETE', route('projects.destroy', $project))->open() }}
-                                            <button type="submit" class="btn btn-white">
-                                                <i class="fas fa-trash-alt"></i>
-                                            </button>
-                                            {{ html()->form()->close() }}
-                                        @endcan
-                                    </div>
-                                </div>
-                                <div class="card-body py-4 border-bottom">
-                                    <p class="card-text">{{ $project->description }}</p>
-                                </div>
-                                <div class="card-footer">
-                                    @if($project->users->count())
-                                        <div class="d-flex">
-                                            @foreach($project->users as $user)
-                                                @if($user->avatar)
-                                                    <img src="{{ $user->avatar_url }}" class="avatar mr-1">
-                                                @else
-                                                    <span class="user-placeholder mr-1">{{ substr($user->name, 0, 2) }}</span>
-                                                @endif
-                                            @endforeach
-                                        </div>
-                                    @endif
-                                </div>
+                                @endcan
+                                @can('delete', $project)
+                                    {{ html()->form('DELETE', route('projects.destroy', $project))->open() }}
+                                    <button type="submit" class="btn btn-white">
+                                        <i class="fas fa-trash-alt"></i>
+                                    </button>
+                                    {{ html()->form()->close() }}
+                                @endcan
                             </div>
                         </div>
-                    @endforeach
-                </div>
-            @endforeach
-
-            {{ $projects->links() }}
+                        <div class="card-body py-4 border-bottom">
+                            <p class="card-text">{{ $project->description }}</p>
+                        </div>
+                        <div class="card-footer">
+                            @if($project->users->count())
+                                <div class="d-flex">
+                                    @foreach($project->users as $user)
+                                        @if($user->avatar)
+                                            <img src="{{ $user->avatar_url }}" class="avatar mr-1">
+                                        @else
+                                            <span class="user-placeholder mr-1">{{ substr($user->name, 0, 2) }}</span>
+                                        @endif
+                                    @endforeach
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                @empty
+                    No Projects
+                @endforelse
+            </div>
         </div>
     </div>
 @endsection
