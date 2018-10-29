0809cb000d9923a7399b3b19aa047c1f55753052
Nikunj Kanetiya <kanetiya@gmail.com>
2018-10-29 22:42:07 +0530

    Update project list UI

--- resources/views/project/list.blade.php
+++ resources/views/project/list.blade.php
@@ -19,40 +19,46 @@
             <div class="card-columns">
                 @forelse($projects as $project)
                     <div class="card mb-3">
-                        <div class="card-header pb-0 border-bottom d-flex justify-content-between">
+                        <div class="card-header pb-0">
                             <a href="{{ $project->path() }}">
                                 <h4 class="card-title">{{ $project->title }}</h4>
+                                <span class="text-reagent-gray">For <strong>{{ $project->client->name }}</strong></span>
                             </a>
+                        </div>
+                        <div class="card-body">
+                            @if($project->users->count())
+                                <div class="d-flex">
+                                    @foreach($project->users as $user)
+                                        <a href="/users/{{ $user->id }}">
+                                            @if($user->avatar)
+                                                <img src="{{ $user->avatar_url }}" class="avatar mr-1">
+                                            @else
+                                                <span class="user-placeholder mr-1">{{ substr($user->name, 0, 2) }}</span>
+                                            @endif
+                                        </a>
+                                    @endforeach
+                                </div>
+                            @endif
+                        </div>
+                        <div class="card-footer border-top text-light d-flex justify-content-between">
+                            <span>
+                                {{ $project->created_at->toDateString() }}
+                            </span>
+
                             <div class="d-flex">
                                 @can('update', $project)
-                                    <a class="btn btn-white" href="/projects/{{ $project->id }}/edit">
-                                        <i class="fas fa-edit"></i>
+                                    <a href="/projects/{{ $project->id }}/edit" class="btn btn-sm btn-white" aria-label="Edit">
+                                        <i class="fas fa-small fa-edit"></i>
                                     </a>
                                 @endcan
                                 @can('delete', $project)
                                     {{ html()->form('DELETE', route('projects.destroy', $project))->open() }}
-                                    <button type="submit" class="btn btn-white">
+                                    <button type="submit" class="btn btn-sm btn-white" aria-label="Delete">
                                         <i class="fas fa-trash-alt"></i>
                                     </button>
                                     {{ html()->form()->close() }}
                                 @endcan
                             </div>
-                        </div>
-                        <div class="card-body py-4 border-bottom">
-                            <p class="card-text">{{ $project->description }}</p>
-                        </div>
-                        <div class="card-footer">
-                            @if($project->users->count())
-                                <div class="d-flex">
-                                    @foreach($project->users as $user)
-                                        @if($user->avatar)
-                                            <img src="{{ $user->avatar_url }}" class="avatar mr-1">
-                                        @else
-                                            <span class="user-placeholder mr-1">{{ substr($user->name, 0, 2) }}</span>
-                                        @endif
-                                    @endforeach
-                                </div>
-                            @endif
                         </div>
                     </div>
                 @empty
