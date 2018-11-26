5ba08ba396626ad6ba6ca6658c5a8353e4caa26e
Nikunj Kanetiya <kanetiya@gmail.com>
2018-11-26 12:38:23 +0530

    Fix migration

--- database/migrations/2018_11_14_111500_drop__designation_to_users_table.php
+++ database/migrations/2018_11_14_111500_drop__designation_to_users_table.php
@@ -14,7 +14,7 @@ class DropDesignationToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('designation');
         });
     }
 
@@ -26,7 +26,7 @@ class DropDesignationToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('designation');
+            $table->string('designation');
         });
     }
 }
