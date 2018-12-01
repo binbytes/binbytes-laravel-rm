e4a8a799fbee4dde505fa077905930956c1d087f
Nikunj Kanetiya <kanetiya@gmail.com>
2018-12-01 19:36:33 +0530

    Update project show view

--- resources/views/project/show.blade.php
+++ resources/views/project/show.blade.php
@@ -5,48 +5,50 @@
 
 @section('content')
     <div class="row">
-        <div class="col-lg-4">
-            <div class="card card-small mb-4 pt-3">
-                <div class="card-header border-bottom text-center">
-                    @if($project->client->avatar)
-                        <div class="mb-3 mx-auto">
-                            <img class="rounded-circle" src="{{ $project->client->avatar_url }}" alt="{{ $project->client->name }}" width="90" height="90">
+        @can('show', $project->client)
+            <div class="col-lg-4">
+                <div class="card card-small mb-4 pt-3">
+                    <div class="card-header border-bottom text-center">
+                        @if($project->client->avatar)
+                            <div class="mb-3 mx-auto">
+                                <img class="rounded-circle" src="{{ $project->client->avatar_url }}" alt="{{ $project->client->name }}" width="90" height="90">
+                            </div>
+                        @endif
+                        <h5 class="mb-0">{{ $project->client->name }}</h5>
+                        <div class="d-block pt-2">
+                            @if($project->client->github)
+                                <a href="{{ $project->client->github }}" class="mx-1"><i class="fab fa-github"></i></a>
+                            @endif
+                            @if($project->client->twitter)
+                                <a href="{{ $project->client->twitter }}" class="mx-1"><i class="fab fa-twitter"></i></a>
+                            @endif
+                            @if($project->client->linkedin)
+                                <a href="{{ $project->client->linkedin }}" class="mx-1"><i class="fab fa-linkedin"></i></a>
+                            @endif
+                            @if($project->client->facebook)
+                                <a href="{{ $project->client->facebook }}" class="mx-1"><i class="fab fa-facebook-f"></i></a>
+                            @endif
                         </div>
-                    @endif
-                    <h5 class="mb-0">{{ $project->client->name }}</h5>
-                    <div class="d-block pt-2">
-                        @if($project->client->github)
-                            <a href="{{ $project->client->github }}" class="mx-1"><i class="fab fa-github"></i></a>
-                        @endif
-                        @if($project->client->twitter)
-                            <a href="{{ $project->client->twitter }}" class="mx-1"><i class="fab fa-twitter"></i></a>
-                        @endif
-                        @if($project->client->linkedin)
-                            <a href="{{ $project->client->linkedin }}" class="mx-1"><i class="fab fa-linkedin"></i></a>
-                        @endif
-                        @if($project->client->facebook)
-                            <a href="{{ $project->client->facebook }}" class="mx-1"><i class="fab fa-facebook-f"></i></a>
-                        @endif
                     </div>
-                </div>
-                <div class="border-top border-bottom p-4">
-                    @if($project->client->company_name)
+                    <div class="border-top border-bottom p-4">
+                        @if($project->client->company_name)
+                            <div class="mb-3">
+                                <h6 class="mb-0">Company</h6>
+                                <span class="text-muted">{{ $project->client->company_name }}</span>
+                            </div>
+                        @endif
                         <div class="mb-3">
-                            <h6 class="mb-0">Company</h6>
-                            <span class="text-muted">{{ $project->client->company_name }}</span>
+                            <h6 class="mb-0">Email <i class="fas fa-envelope-open"></i></h6>
+                            <span class="text-muted">{{ $project->client->email ?? '---'}}</span>
                         </div>
-                    @endif
-                    <div class="mb-3">
-                        <h6 class="mb-0">Email <i class="fas fa-envelope-open"></i></h6>
-                        <span class="text-muted">{{ $project->client->email ?? '---'}}</span>
-                    </div>
-                    <div>
-                        <h6 class="mb-0">Phone <i class="fas fa-phone"></i></h6>
-                        <span class="text-muted">{{ $project->client->mobile_no ?? '---' }}</span>
+                        <div>
+                            <h6 class="mb-0">Phone <i class="fas fa-phone"></i></h6>
+                            <span class="text-muted">{{ $project->client->mobile_no ?? '---' }}</span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endcan
         <div class="col-lg-8">
             <div class="card card-small mb-4">
                 <div class="card-header border-bottom pl-4">
