8ba373ba62e8119f1c6936adf66922d3fc009240
Nikunj Kanetiya <kanetiya@gmail.com>
2018-12-27 14:27:59 +0530

    Remove telescope

--- config/app.php
+++ config/app.php
@@ -159,7 +159,6 @@ return [
         App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
     ],
